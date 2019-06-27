89a402369abef76aef00b85985c41ca8e1c59e7c
alexgfaria <alex.faria@me.com>
2019-06-27 14:40:44 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,78 +1,106 @@
-const rings = document.querySelectorAll('.ring'),
-    spin  = document.querySelector('.spin'),
-    degs  = [0, 45, 90, 135, 180, 225, 270, 315, 360]
+const SLOTS_PER_REEL = 12;
+// radius = Math.round( ( panelWidth / 2) / Math.tan( Math.PI / SLOTS_PER_REEL ) ); 
+// current settings give a value of 149, rounded to 150
+const REEL_RADIUS = 150;
 
-window.onload = () => {
-    rings.forEach((el, i) => {
-        let obj = {},
-            arr = obj.arr = []
+function createSlots (ring) {
+	
+	var slotAngle = 360 / SLOTS_PER_REEL;
 
-        el.querySelectorAll('.slot').forEach((el) => {
-            let id = el.getAttribute('id')
-            obj.arr.push(id)
-        })
+	var seed = getSeed();
 
-        obj.next = arr[arr.length-2]
-        obj.curr = arr[arr.length-1]
-        obj.prev = arr[0]
-        obj.deg = 0
+	for (var i = 0; i < SLOTS_PER_REEL; i ++) {
+		var slot = document.createElement('div');
+		
+		slot.className = 'slot';
 
-        window[`ring${i}`] = obj
-    })
+		// compute and assign the transform for this slot
+		var transform = 'rotateX(' + (slotAngle * i) + 'deg) translateZ(' + REEL_RADIUS + 'px)';
 
-    update()
+		slot.style.transform = transform;
+
+		// setup the number to show inside the slots
+		// the position is randomized to 
+
+		var content = $(slot).append('<p>' + ((seed + i)%12)+ '</p>');
+
+		// add the poster to the row
+		ring.append(slot);
+	}
 }
 
-spin.addEventListener('click', rotate)
-
-function rotate() {
-    rings.forEach((el, i) => {
-        let obj  = window[`ring${i}`],
-            deg  = obj.deg,
-            curr = obj.curr,
-            arr  = obj.arr,
-            res  = deg - degs[rnd(0, degs.length)] * rnd(1, 30)
-
-        el.style.transform = `rotateX(${res}deg)`
-        obj.deg = res
-
-        let cnt = Math.abs(res - deg) / (360/arr.length),
-            tmp = arr.slice(arr.indexOf(curr)),
-            next,
-            prev
-
-        for (let i = 0; i <= cnt; i++) {
-            tmp.push.apply(tmp, arr)
-            curr = tmp[i]
-            next = tmp[i - 1]
-            prev = tmp[i + 1]
-        }
-
-        obj.curr = curr
-
-        if (cnt > 0) {
-            obj.next = next
-            obj.prev = prev
-        }
-
-        window[`ring${i}`] = obj
-    })
-
-    setTimeout(() => {
-        update()
-    }, 2825)
+function getSeed() {
+	// generate random number smaller than 13 then floor it to settle between 0 and 12 inclusive
+	return Math.floor(Math.random()*(SLOTS_PER_REEL));
 }
 
-function update() {
-    const results = document.querySelector('.results')
+function spin(timer) {
+	//var txt = 'seeds: ';
+	for(var i = 1; i < 6; i ++) {
+		var oldSeed = -1;
+		/*
+		checking that the old seed from the previous iteration is not the same as the current iteration;
+		if this happens then the reel will not spin at all
+		*/
+		var oldClass = $('#ring'+i).attr('class');
+		if(oldClass.length > 4) {
+			oldSeed = parseInt(oldClass.slice(10));
+			console.log(oldSeed);
+		}
+		var seed = getSeed();
+		while(oldSeed == seed) {
+			seed = getSeed();
+		}
 
-    results.innerHTML = ''
-    results.insertAdjacentHTML('beforeend',
-        `<div class="result">${ring0.next} ${ring1.curr} ${ring2.prev}</div>
-		 	 <div class="result">${ring0.curr} ${ring1.curr} ${ring2.curr}</div>
-			 <div class="result">${ring0.prev} ${ring1.curr} ${ring2.next}</div>`)
+		$('#ring'+i)
+			.css('animation','back-spin 1s, spin-' + seed + ' ' + (timer + i*0.5) + 's')
+			.attr('class','ring spin-' + seed);
+	}
+
+	console.log('=====');
 }
 
-function rnd(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min
-}
+$(document).ready(function() {
+
+	// initiate slots 
+ 	createSlots($('#ring1'));
+ 	createSlots($('#ring2'));
+ 	createSlots($('#ring3'));
+ 	createSlots($('#ring4'));
+ 	createSlots($('#ring5'));
+
+ 	// hook start button
+ 	$('.go').on('click',function(){
+ 		var timer = 2;
+ 		spin(timer);
+ 	})
+
+ 	// hook xray checkbox
+ 	$('#xray').on('click',function(){
+ 		//var isChecked = $('#xray:checked');
+ 		var tilt = 'tiltout';
+ 		
+    if($(this).is(':checked')) {
+ 			tilt = 'tiltin';
+ 			$('.slot').addClass('backface-on');
+ 			$('#rotate').css('animation',tilt + ' 2s 1');
+
+			setTimeout(function(){
+			  $('#rotate').toggleClass('tilted');
+			},2000);
+ 		} else {
+      tilt = 'tiltout';
+ 			$('#rotate').css({'animation':tilt + ' 2s 1'});
+
+			setTimeout(function(){
+	 			$('#rotate').toggleClass('tilted');
+	 			$('.slot').removeClass('backface-on');
+	 		},1900);
+ 		}
+ 	})
+
+ 	// hook perspective
+ 	$('#perspective').on('click',function(){
+ 		$('#stage').toggleClass('perspective-on perspective-off');
+ 	})	
+ });
