f06fa8bab5e7452c55ea4cb8a92e3298d10285eb
alexgfaria <alex.faria@me.com>
2019-08-26 16:40:14 +0100

    sidebar

--- js/main.js
+++ js/main.js
@@ -98,3 +98,18 @@ const button = document.querySelector('button');
 button.addEventListener('click', _ => {
   document.getElementById('sidebar').classList.toggle('collapsed');
 })
+
+
+
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
